66e2a860ebd041407c85971e90cee070a7db2f17
Murillo R. Araújo <mr_araujo@icloud.com>
2022-09-30 16:58:56 -0300

    Initial Commit

---
+++ Petlove SwiftUI/Petlove_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Petlove_SwiftUIApp.swift
+//  Petlove SwiftUI
+//
+//  Created by Murillo R. Araújo on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Petlove_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
