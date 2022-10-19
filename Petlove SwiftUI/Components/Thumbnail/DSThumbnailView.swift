58896f5c0fdaa661ff46d8c045e1eabbd1e3083b
Murillo R. Araújo <mr_araujo@icloud.com>
2022-10-19 15:19:26 -0300

    Improve DS Thumbnail

--- Petlove SwiftUI/Components/Thumbnail/DSThumbnailView.swift
+++ Petlove SwiftUI/Components/Thumbnail/DSThumbnailView.swift
@@ -7,42 +7,95 @@
 
 import SwiftUI
 
+struct Thumbnail {
+
+    public enum Size {
+        /// Thumbnail size: 48
+        case extraSmall
+        /// Thumbnail size: 56
+        case small
+        /// Thumbnail size: 56
+        case medium
+        /// Thumbnail size: 64
+        case large
+        /// Thumbnail size: 96
+        case extraLarge
+        /// Thumbnail size: 120
+        case giant
+    }
+
+    public enum Border {
+        case outline
+        case none
+    }
+}
+
 struct DSThumbnailView: View {
     let image: String
-    let count: Int
+    let quantity: Int
+    let size: Thumbnail.Size
+    let border: Thumbnail.Border
+
+    init(image: String, quantity: Int, size: Thumbnail.Size, border: Thumbnail.Border = .none) {
+        self.image = image
+        self.quantity = quantity
+        self.size = size
+        self.border = border
+    }
 
     var body: some View {
         ZStack {
             Image(image)
                 .resizable()
-                .frame(width: 48, height: 48)
+                .frame(width: getProperties().dimension, height: getProperties().dimension)
 
             HStack {
                 VStack {
                     Spacer()
 
                     ZStack {
-                      Circle()
+                        Circle()
                             .fill(Color.primaryMain)
 
-                      Text("\(count)")
+                        Text("\(quantity)")
                             .padding(.horizontal, 2)
-                            .designSystemFont(.extraSmallBody, .bold)
+                            .designSystemFont(getProperties().font, .bold)
                             .foregroundColor(.white)
                             .minimumScaleFactor(0.7)
-                    }.frame(width: 20, height: 20)
+                    }.frame(width: getProperties().dimension * 0.45, height: getProperties().dimension * 0.45)
                 }
                 Spacer ()
             }
         }
-        .padding()
-        .frame(width: 48, height: 48)
+        .frame(width: getProperties().dimension, height: getProperties().dimension)
+        .padding(12)
+        .overlay(
+            RoundedRectangle(cornerRadius: getProperties().border)
+                .stroke(Color.neutralLightest, lineWidth: border == .outline ? 1 : 0)
+        )
+    }
+
+    private func getProperties() -> (dimension: CGFloat, border: CGFloat, font: FontSize) {
+        switch size {
+        case .extraSmall:
+            return (48, 8, .extraSmallBody)
+        case .small:
+            return (56, 8, .extraSmallBody)
+        case .medium:
+            return (64, 12, .smallBody)
+        case .large:
+            return (80, 12, .body)
+        case .extraLarge:
+            return (96, 16, .extraSmallTitle)
+        case .giant:
+            return (120, 16, .smallTitle)
+        }
     }
 }
 
 struct DSThumbnailView_Previews: PreviewProvider {
     static var previews: some View {
-        DSThumbnailView(image: "True", count: 9)
-            .previewLayout(.fixed(width: 100, height: 100))
+        DSThumbnailView(image: "True", quantity: 99, size: .giant, border: .outline)
+            .previewLayout(.fixed(width: 200, height: 200))
     }
 }
